e08ca411221468c69e123704ff1cefc4f9425216
IsaAliev <isaaliev12@gmail.com>
2019-11-02 12:39:05 +0300

    Initial Commit

---
+++ MVVM Properties and Commands/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM Properties and Commands
+//
+//  Created by Isa Aliev on 02/11/2019.
+//  Copyright © 2019 IA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
